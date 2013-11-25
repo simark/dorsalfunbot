d101749ff61d23597f3484cbf128bd95ac1ea132
simark <thundersim@gmail.com>
2013-11-25 11:03:31 -0500

    Crazy with negative values

--- crazy.py
+++ crazy.py
@@ -5,9 +5,10 @@ class Crazy:
 	def __init__(self, irc):
 		self.irc = irc
 		self.max_lvl = 11
+		self.min_lvl = -self.max_lvl
 	
 	def usage(self, chan, from_):
-		self.irc.privmsg(chan, from_[0] + ": the crazyness level must be an int from 0 to " + str(self.max_lvl))
+		self.irc.privmsg(chan, from_[0] + ": the crazyness level must be an int from %d to %d" % (self.min_lvl, self.max_lvl))
 		
 
 	def on_chanmsg(self, from_, chan, msg):
@@ -18,6 +19,8 @@ class Crazy:
 
 		level_str = parts[1]
 		topic = self.irc.topic(chan)[0]
+		topic = self.irc.topic(chan)[0]
+		#topic = self.irc.topic(chan)[0]
 
 		try:
 			current_level = len(re.findall("\[(=*)-*\]", topic)[0])
@@ -31,7 +34,7 @@ class Crazy:
 			else:
 				level = current_level
 		elif level_str == "--":
-			if current_level > 0:
+			if current_level > self.min_lvl:
 				level = current_level - 1
 			else:
 				level = current_level
@@ -39,17 +42,19 @@ class Crazy:
 
 			try:
 				level = int(level_str)
-				if level < 0 or level > self.max_lvl:
+				if level < self.min_lvl or level > self.max_lvl:
 					self.usage(chan, from_)
 					return
 			except ValueError:
 				self.usage(chan, from_)
-				return		
-		crazy_str = "[" + "=" * level  + "-" * (self.max_lvl - level) + "]"
-		new_topic = re.sub("(\[=+-+\]|\[=+\]|\[-+\])", crazy_str, topic)
+				return
+		the_char = "=" if level > 0 else "_"
+		level = abs(level)
+		crazy_str = "[" + the_char * level  + "-" * (self.max_lvl - level) + "]"
+		new_topic = re.sub("(\[[=_]+-+\]|\[[=_]+\]|\[-+\])", crazy_str, topic)
 		#self.irc.privmsg(chan, "New topic would be " + new_topic)
 		try:
 			self.irc.topic(chan, new_topic)
 		except lurklib.exceptions._Exceptions.ChanOPrivsNeeded as e:
-			self.irc.privmsg(chan, "I require OP, OP, OP")
+			self.irc.privmsg(chan, "I require OP, OP, OP -> /mode #dorsal-fun +o DorsalFunBot")
 
