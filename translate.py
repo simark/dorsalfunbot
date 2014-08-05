408cca4713218a89af3601ee2e698aa96c070b0a
Suchakra Sharma <suchakrapani.sharma@polymtl.ca>
2014-08-05 17:54:14 -0400

    Initial translation tryout

---
+++ translate.py
@@ -0,0 +1,23 @@
+import goslate
+
+class Translate:
+	def __init__(self, irc):
+		self.irc = irc
+
+	def on_chanmsg(self, from_, chan, msg):
+		translated Translate.get_translation()
+		if (translated == None):
+            self.irc.privmsg(chan, "Could not translate...")
+		else:
+            self.irc.privmsg(chan, "En Anglais : " + translated)
+	
+	def get_translation():
+		try:
+		    gs = goslate.Goslate()
+            trans_text = gs.translate('Se creuser la tête', 'en')
+			return trans_text
+		except:
+			return None
+
+	def halp(self):
+		return "!translate : Translate last statement to English";
