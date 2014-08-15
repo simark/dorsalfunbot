160f6b1ec203936e742ba3513f86879e04162023
Suchakra Sharma <suchakrapani.sharma@polymtl.ca>
2014-08-15 18:51:11 +0000

    Translate language features Detect language of statement and inform user

--- translate.py
+++ translate.py
@@ -1,26 +1,34 @@
 import goslate
 import globals
 
-
 class Translate:
+	def __init__(self, irc):
+		self.irc = irc
 
-    def __init__(self, irc):
-        self.irc = irc
+	def on_chanmsg(self, from_, chan, msg):
+		translated = Translate.get_translation()
+		if (translated == None):
+			self.irc.privmsg(chan, "Could not translate...")
+		else:
+			self.irc.privmsg(chan, translated)
 
-    def on_chanmsg(self, from_, chan, msg):
-        translated = Translate.get_translation()
-        if (translated == None):
-            self.irc.privmsg(chan, "Could not translate...")
-        else:
-            self.irc.privmsg(chan, "En Anglais : " + translated)
+	def get_translation():
+		try:
+			gs = goslate.Goslate()
+			from_lang = gs.detect(globals.g_buffmsg)
+			if from_lang == 'fr':
+				to_lang = 'en'
+				header = 'Fr -> En : '
+			elif from_lang == 'en':
+				to_lang = 'fr'
+				header = 'En -> Fr : '
+			else:
+				to_lang = 'en'
+				header = from_lang+' -> En : '
+			trans_text = gs.translate(globals.g_buffmsg, to_lang)
+			return header+trans_text
+		except:
+			return None
 
-    def get_translation():
-        try:
-            gs = goslate.Goslate()
-            trans_text = gs.translate(globals.g_buffmsg, 'en')
-            return trans_text
-        except:
-            return None
-
-    def halp(self):
-        return "!translate : Translate last statement to English"
+	def halp(self):
+		return "!translate : Translate last statement to English"
